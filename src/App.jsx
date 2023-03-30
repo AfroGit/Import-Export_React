5936c17dcf43caf6fbf38fabe1a2951fa4eca62f
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-30 03:41:04 +0000

    The double {{ }} is used as both an escape and a template literal for objects and react functions.

--- src/App.jsx
+++ src/App.jsx
@@ -61,27 +61,27 @@ export default function Gallery() {
 
 
 
-<section className="profile">
-<h2>Maria Skłodowska-Curie</h2>
-<img 
-  className="avatar"
-  src={getImageUrl('szV5sdG')}
-  alt="Maria Skłodowska-Curie"
-  width={70}
-  height={70}
-/>
-<ul>
-  <li>
-    <b>Profession: </b> 
-    physicist and chemist
-  </li>
-  <li>
-    <b>Awards: 4 </b> 
-    (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-  </li>
-  <li>
-    <b>Discovered: </b>
-    polonium (element)
-  </li>
-</ul>
-</section>
+// <section className="profile">
+// <h2>Maria Skłodowska-Curie</h2>
+// <img 
+//   className="avatar"
+//   src={getImageUrl('szV5sdG')}
+//   alt="Maria Skłodowska-Curie"
+//   width={70}
+//   height={70}
+// />
+// <ul>
+//   <li>
+//     <b>Profession: </b> 
+//     physicist and chemist
+//   </li>
+//   <li>
+//     <b>Awards: 4 </b> 
+//     (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
+//   </li>
+//   <li>
+//     <b>Discovered: </b>
+//     polonium (element)
+//   </li>
+// </ul>
+// </section>
