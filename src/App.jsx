080863ba1892799c009feb5c32ad40e78ce1e147
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-30 22:02:02 +0000

    Passing multiple props is easy if they have are flexible and adaptable

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,20 @@ export default function Gallery() {
           
         ]
       }} />
-{/*       <Profile /> */}
+      <Profile person= {{
+     imageId: 'YfeOqp2',
+        name: "Katsuko Saruhashi",
+        profession: 'geochemist',
+        discovery: 'a method for measuring carbon dioxide in seawater',
+        awards: [
+          'Miyake Prize for geochemistry',
+          'Tanaka Prize'
+          
+        ]
+
+
+
+}}/>
       
     </div>
   );
@@ -61,27 +74,4 @@ export default function Gallery() {
 
 
 
-// <section className="profile">
-// <h2>Maria Skłodowska-Curie</h2>
-// <img 
-//   className="avatar"
-//   src={getImageUrl('szV5sdG')}
-//   alt="Maria Skłodowska-Curie"
-//   width={70}
-//   height={70}
-// />
-// <ul>
-//   <li>
-//     <b>Profession: </b> 
-//     physicist and chemist
-//   </li>
-//   <li>
-//     <b>Awards: 4 </b> 
-//     (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-//   </li>
-//   <li>
-//     <b>Discovered: </b>
-//     polonium (element)
-//   </li>
-// </ul>
-// </section>
+
